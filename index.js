e3e960195bb58a65b200bcf82955200ef7084862
bharathps123 <b1997oct@gmail.com>
2024-01-06 16:08:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+
+export function Menu({ width = '24', height = '24' }) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" height={`${height}`} width={`${width}`} viewBox="0 0 24 24"  fill="currentColor"><path d="M0 0h24v24H0z" fill="none" /><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" /></svg>
+  )
+}
+
+export function DriveFileRenameOutline({ width = '24', height = '24' }) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" height={`${height}`} width={`${width}`} viewBox="0 0 24 24" fill="currentColor"><path d="M0 0h24v24H0z" fill="none" /><path d="M18.41 5.8L17.2 4.59c-.78-.78-2.05-.78-2.83 0l-2.68 2.68L3 15.96V20h4.04l8.74-8.74 2.63-2.63c.79-.78.79-2.05 0-2.83zM6.21 18H5v-1.21l8.66-8.66 1.21 1.21L6.21 18zM11 20l4-4h6v4H11z" /></svg>
+  )
+}
+
+export function LeftArrow({ width = '24', height = '24' }) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" height={`${height}`} width={`${width}`} viewBox="0 0 24 24" fill="currentColor"><rect fill="none" height="24" width="24" /><g><polygon points="17.77,3.77 16,2 6,12 16,22 17.77,20.23 9.54,12" /></g></svg>
+  )
+}
+
+export function RightArrow({ width = '24', height = '24' }) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" height={`${height}`} width={`${width}`} viewBox="0 0 24 24" fill="currentColor"><g><path d="M0,0h24v24H0V0z" fill="none" /></g><g><polygon points="6.23,20.23 8,22 18,12 8,2 6.23,3.77 14.46,12" /></g></svg>
+  )
+}
+
+export function Cached({ width = '24', height = '24' }) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" height={`${height}`} width={`${width}`} viewBox="0 0 24 24" fill="currentColor"><path d="M0 0h24v24H0z" fill="none" /><path d="M19 8l-4 4h3c0 3.31-2.69 6-6 6-1.01 0-1.97-.25-2.8-.7l-1.46 1.46C8.97 19.54 10.43 20 12 20c4.42 0 8-3.58 8-8h3l-4-4zM6 12c0-3.31 2.69-6 6-6 1.01 0 1.97.25 2.8.7l1.46-1.46C15.03 4.46 13.57 4 12 4c-4.42 0-8 3.58-8 8H1l4 4 4-4H6z" /></svg>
+  )
+}
+
+export function Camera({ width = '24', height = '24' }) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" height={`${height}`} width={`${width}`} viewBox="0 0 24 24" fill="currentColor"><path d="M0 0h24v24H0z" fill="none" /><circle cx="12" cy="12" r="3.2" /><path d="M9 2L7.17 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2h-3.17L15 2H9zm3 15c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z" /></svg>
+  )
+}
+
+export function Close({ width = '24', height = '24' }) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" height={`${height}`} width={`${width}`} viewBox="0 0 24 24"  fill="currentColor"><path d="M0 0h24v24H0z" fill="none" /><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" /></svg>
+  )
+}
+
+export function Cancel({ width = '24', height = '24' }) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" height={`${height}`} width={`${width}`} viewBox="0 0 24 24" fill="currentColor"><path d="M0 0h24v24H0z" fill="none" /><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z" /></svg>
+  )
+}
